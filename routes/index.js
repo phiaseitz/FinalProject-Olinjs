c70d4a747c09ab7366682184a64881bbedb96430
Samantha Kumarasena <samantha.kumarasena@students.olin.edu>
2016-03-29 15:42:29 -0400

    Minor changes -- no need to sort

--- routes/index.js
+++ routes/index.js
@@ -49,10 +49,12 @@ var getWeekMealsGET = function(req, res) {
     //need callback after loop is done!
     Meal.find({ 'date': { $in: weekdates} }, function(err, meals){
         console.log(meals);
-    }).sort({date: 1}).exec(function(err, meals) {  //sort by ascending date
-        console.log(meals);     //but what about meal sorting? b, l, d order...
         res.send(meals);
     });
+    // }).sort({date: 1}).exec(function(err, meals) {  //sort by ascending date
+    //     console.log(meals);     //but what about meal sorting? b, l, d order...
+    //     res.send(meals);
+    // });
 
 }
 
