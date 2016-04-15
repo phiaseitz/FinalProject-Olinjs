a5ba0b47039b670321e114d183dd1a525b3d37a7
Mac-I Crowell <macicrowell@gmail.com>
2016-04-15 14:44:42 -0400

    added nutrition to mongoose schema

--- models/foodModel.js
+++ models/foodModel.js
@@ -1,5 +1,35 @@
 var mongoose = require('mongoose');
 
+var nutritionSchema = new mongoose.Schema({ 	serving: String,
+									calories: Number,
+									fatCalories: Number,
+									fat: Number,
+									fatPercent: Number,
+									saturatedFat: Number,
+									saturatedFatPercent: Number,
+									transFat: Number,
+									cholesterol: Number,
+									cholesterolPercent: Number,
+									sodium: Number,
+									sodiumPercent: Number,
+									carbohydrates: Number,
+									carbohydratesPercent: Number,
+									dietaryFiber: Number,
+									dietaryFiberPercent: Number,
+									sugar: Number,
+									protein: Number,
+									vitAPercent: Number,
+									vitCPercent: Number,
+									calciumPercent:Number,
+									ironPercent:Number,
+									name: String,
+									description: String,
+									allergens: [String],
+									vitA: Number,
+									vitC: Number,
+									calcium: Number,
+									iron: Number});
+
 // Create a Schema
 var foodSchema = mongoose.Schema({
     name: String,
@@ -8,6 +38,7 @@ var foodSchema = mongoose.Schema({
     vegan: { type: Boolean, default: false },
 	vegetarian: { type: Boolean, default: false },
 	mindful: { type: Boolean, default: false },
+	nutritionInformation: [nutritionSchema],
     lastUpdated: { type: Date, default: Date.now }
 });
 
